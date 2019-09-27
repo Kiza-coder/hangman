cc726d7c9ce362fd1e06ec8ca530730905a2c318
Kiza-coder <kiza.motoko@gmail.com>
2019-09-27 10:46:24 +0200

    Update script.js

--- script2.js
+++ script2.js
@@ -96,9 +96,7 @@ function recupLetter(tabGuessLetter)
 	let letter = document.getElementById("letter").value;
 	if(letter.length !=1)
 	{
-		letter = "$"
-		alert("Veuillez rentrer une et une seule lettre");
-
+		
 	}
 	else{
 		for(let i=0; i<tabGuessLetter.length;i++)
@@ -130,42 +128,153 @@ function compareFalseLetter(letter,tabGuessLetter)
 }
 
 
+function compareWord(word,newWord)
+{
+	return (word.toUpperCase() == newWord.toUpperCase());
+}
+
+//funnction separate in parts for display the hangover
+function displayHangover()
+{
+	let canvas = document.getElementById('canvas');
+ 	if (canvas.getContext) 
+ 	{
+  		let ctx = canvas.getContext('2d');
+  		ctx.beginPath();
+	    ctx.moveTo(60,150)
+	    ctx.lineTo(240,150)
+	    ctx.moveTo(220,150)
+	    ctx.lineTo(220,30)
+	    ctx.lineTo(120,30)
+	    ctx.lineTo(120,60)
+	    ctx.moveTo(220,60)
+	    ctx.lineTo(160,30)	
+	    ctx.stroke()
+	} 
+}
+
+
+function displayHead()
+{
+	let canvas = document.getElementById('canvas');
+ 	if (canvas.getContext) 
+ 	{
+  		let ctx = canvas.getContext('2d');
+  		ctx.beginPath()
+  		ctx.moveTo(120,60)
+  		ctx.arc(120, 60, 10, 50, 0, 2 * Math.PI);
+  		ctx.fill()
+	} 
+}
+
+
+function displayBody()
+{
+	let canvas = document.getElementById('canvas');
+ 	if (canvas.getContext) 
+ 	{
+  		let ctx = canvas.getContext('2d');
+  		ctx.beginPath()
+  		ctx.moveTo(120,60)
+  		ctx.lineTo(120,100);
+  		ctx.stroke()
+	} 
+}
+
+
+function displayArm(num)
+{
+let canvas = document.getElementById('canvas');
+ 	if (canvas.getContext) 
+ 	{
+  		let ctx = canvas.getContext('2d');
+  		ctx.beginPath()
+  		ctx.moveTo(120,80)
+  		ctx.lineTo(num,85);
+  		ctx.stroke()
+	} 
+
+}
+
+function displayLeg(num)
+{
+let canvas = document.getElementById('canvas');
+
+ 	if (canvas.getContext) 
+ 	{
+  		let ctx = canvas.getContext('2d');
+  		ctx.beginPath()
+  		ctx.moveTo(120,100)
+  		ctx.lineTo(num,105);
+  		ctx.stroke()
+	} 
+
+}
 
 
 
+// function principale
+function main()
+{
+	
+	setInterval(function(){
+		switch (lives) {
+  		case 6:displayHangover()
 
+    	break;
+    	case 5:displayHangover()
+				displayHead()
+    	break;
+    	case 4:displayHangover()
+				displayHead()
+				displayBody()
+    	break;
+    	case 3:displayHangover()
+				displayHead()
+				displayBody()
+				displayArm(100)
+    	break;
+    	case 2:displayHangover()
+				displayHead()
+				displayBody()
+				displayArm(100)
+				displayArm(140)
+    	break;
+    	case 1:displayHangover()
+				displayHead()
+				displayBody()
+				displayArm(100)
+				displayArm(140)
+				displayLeg(135)
+    	break;
+ 	 	default:displayHangover()
+				displayHead()
+				displayBody()
+				displayArm(100)
+				displayArm(140)
+				displayLeg(135)
+				displayLeg(105)
+				console.log("Perdu")
+		}
+    },100);
+}
 
+document.getElementById("run-letter").addEventListener("click",() =>{
+	let letter = recupLetter(tabGuessLetter); 
 
-
-
-
-
-
-	let lives =6
-	let tabGuessLetter = new Array();
-	let guess
-	let cptGuess =0
-	let word="manger"
-	let tabWordBoolean = newTabWord(word);
-	console.log(tabWordBoolean)
-	displayWord(word,tabWordBoolean);
-
-
-
-
-
-
-
-document.getElementById("run").addEventListener("click",() =>{
-	let letter = recupLetter(tabGuessLetter);
-	if(!compareFalseLetter(letter,tabGuessLetter))
+	if(!compareFalseLetter(letter,tabGuessLetter) || letter == "")
 	{
-		alert("Lettre deja cité");
+		if(letter == "")
+		{
+			alert("Lettre inexistante")
+		}
+		else
+		{ 
+			alert("Lettre deja cité");
+		}
 	}
-
 	else
-	{
-		
+	{	
 		if(compareLetters(word,letter,tabGuessLetter))
 		{
 			changeWord(tabWordBoolean,letter,word);
@@ -173,18 +282,36 @@ document.getElementById("run").addEventListener("click",() =>{
 			alert("Bonne lettre");
 			displayWord(word,tabWordBoolean);
 		}
-
 		else
 		{			
 			guess = "g-"+ cptGuess
 			alert("Mauvaise lettre")
-			document.getElementById(guess).innerHTML = letter
+			document.getElementById(guess).innerHTML = letter.toUpperCase()
 			lives --;
-			cptGuess++
+			cptGuess++;
 			document.getElementById("lives").innerHTML = "LIVES : "+lives;
 		}
 	}
-	console.log(tabGuessLetter)
-
+	document.getElementById("letter").value ="";
+	document.getElementById("letter").focus();
 });
 
+document.getElementById("run-word").addEventListener("click",() =>{
+	if(compareWord(word,document.getElementById("word").value)){
+	console.log("win")
+	
+}
+	document.getElementById("word").value ="";
+	document.getElementById("word").focus();
+});
+
+
+    let lives =6
+	let tabGuessLetter = new Array();
+	let guess
+	let cptGuess =0
+	let word="manger" 
+	let tabWordBoolean = newTabWord(word);
+	console.log(tabWordBoolean)
+	displayWord(word,tabWordBoolean);
+	main();	
